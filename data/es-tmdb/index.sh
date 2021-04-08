89e740d002d1027dc3463763334b90e0106ae35b
jzonthemtn <jeff.zemerick@mtnfog.com>
2021-04-08 10:46:10 -0400

    Removing files.

--- data/es-tmdb/index.sh
+++ data/es-tmdb/index.sh
@@ -7,6 +7,12 @@ if [ ! -f ./tmdb_es.json ]; then
   unzip tmdb_es.json.zip
 fi
 
-curl -XDELETE "http://$HOST:9200/$INDEX";
-curl -XPUT "http://$HOST:9200/$INDEX/" -H 'Content-Type: application/json' --data-binary @schema.json
-curl -XPOST "http://$HOST:9200/$INDEX/_bulk" -H 'Content-Type: application/json' --data-binary @tmdb_es.json
+# Delete and create the index.
+curl -X DELETE "http://$HOST:9200/$INDEX"
+curl -X PUT "http://$HOST:9200/$INDEX/" -H "Content-Type: application/json" --data-binary @schema.json
+
+# Index the movies.
+curl -X POST "http://$HOST:9200/$INDEX/_bulk" -H "Content-Type: application/json" --data-binary @tmdb_es.json
+
+rm tmdb_dump_2020-12-29.json
+rm tmdb_es.json
